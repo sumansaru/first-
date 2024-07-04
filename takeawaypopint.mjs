8eed722f3de73b89da33f95b7ed1df4def3a6e3e
suman <sumansaru231@gmail.com>
2024-07-04 09:44:35 +0545

    first commit gareko

---
+++ takeawaypopint.mjs
@@ -0,0 +1,24 @@
+import App from "./src/App"
+
+// instalation
+npm create vite@laste react1
+
+url 
+local host :5173
+
+how ho run react App
+npm run dev
+
+console 
+console will appear at developer pannel
+
+
+any thing which is write inside return are printed on browser
+
+
+import 
+    import { Component } from "react"
+export
+
+
+Component
